928a73c424024a3e662d390b0fc3ba260621a4e0
jacf <jacfpentester@gmail.com>
2023-05-12 23:28:21 +0200

    commit 2

---
+++ pTienda/dslTienda.groovy
@@ -0,0 +1,22 @@
+// import jenkins-auto-approve-script.groovy
+
+job('Job-pTienda') {
+    description('Job Padre para crear una tarea Hija que despliegue la App Tienda del Corte Ingles')
+    scm {
+        git('https://github.com/morgadodesarrollador/jenkinsPTienda.git', 'master'){ 
+            node -> 
+                node / gitConfigName('morgadodesarrollador')
+                node / gitConfigEmail('morgadodesarrollador@gmail.com')
+        }
+    }
+    triggers {
+      //  scm('H/5 * * * *')
+            //  scm('H/5 * * * *')
+
+        githubPush()
+    }
+    
+    steps {
+        shell("bash levantar.sh")
+    }
+}
